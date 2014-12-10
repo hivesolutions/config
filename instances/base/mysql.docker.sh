ef62ea43e2ad92c82800be9f3da3689343a8d6fb
joamag <joamag@gmail.com>
2014-12-10 20:36:53 +0000

    new strict execution

--- instances/base/mysql.docker.sh
+++ instances/base/mysql.docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
+set -e +h
+
 mkdir mysql && cd mysql
 wget https://github.com/hivesolutions/config/raw/master/docker/mysql/Dockerfile -O Dockerfile
+docker pull ubuntu
 docker build --no-cache -t self/mysql .
 cd .. && rm -rf mysql
