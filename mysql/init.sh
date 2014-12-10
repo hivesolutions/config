a17db0809c63ff72f5175038d42f48d5bf71247c
joamag <joamag@gmail.com>
2014-12-10 20:41:41 +0000

    small code fix

--- mysql/init.sh
+++ mysql/init.sh
@@ -4,7 +4,7 @@
 /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
 RET=1
-while [[ RET -ne 0 ]]; do
+while [[ $RET -ne 0 ]]; do
     echo "=> Waiting for confirmation of MySQL service startup"
     sleep 5
     mysql -uroot -e "status" > /dev/null 2>&1
