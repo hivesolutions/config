2717ebc407335a454c70e7c1b8b50966511db1f7
joamag <joamag@gmail.com>
2014-12-10 20:43:14 +0000

    new code

--- mysql/init.sh
+++ mysql/init.sh
@@ -5,7 +5,7 @@
 
 RET=1
 while [[ $RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MySQL service startup"
+    echo "=> Waiting for confirmation of MySQL service startup ($RET)"
     sleep 5
     mysql -uroot -e "status" > /dev/null 2>&1
     RET=$?
