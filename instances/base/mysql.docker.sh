3720086595ddb2072c933139b5f1b9e55cdcc152
joamag <joamag@gmail.com>
2014-12-11 17:41:40 +0000

    new base building

--- instances/base/mysql.docker.sh
+++ instances/base/mysql.docker.sh
@@ -3,12 +3,12 @@
 
 set -e +h
 
-HOST=${HOST-172.17.42.1}
-PORT=${PORT-3306}
+MYSQL_HOST=${MYSQL_HOST-172.17.42.1}
+MYSQL_PORT=${MYSQL_PORT-3306}
 
 mkdir mysql && cd mysql
 wget https://github.com/hivesolutions/config/raw/master/docker/mysql/Dockerfile -O Dockerfile
 docker pull ubuntu && docker build --no-cache -t self/mysql .
 cd .. && rm -rf mysql
 
-docker run --name mysql -p $HOST:$PORT:$PORT -v /data:/data -i -t -d self/mysql
+docker run --name mysql -p $MYSQL_HOST:$MYSQL_PORT:$MYSQL_PORT -v /data:/data -i -t -d self/mysql
--- instances/base/redis.docker.sh
+++ instances/base/redis.docker.sh
@@ -3,12 +3,12 @@
 
 set -e +h
 
-HOST=${HOST-172.17.42.1}
-PORT=${PORT-6379}
+REDIS_HOST=${REDIS_HOST-172.17.42.1}
+REDIS_PORT=${REDIS_PORT-6379}
 
 mkdir redis && cd redis
 wget https://github.com/hivesolutions/config/raw/master/docker/redis/Dockerfile
 docker build --no-cache -t self/redis .
 cd .. && rm -rf redis
 
-docker run --name redis -p $HOST:$PORT:$PORT -v /data:/data -i -t self/redis
+docker run --name redis -p $REDIS_HOST:$REDIS_PORT:$REDIS_PORT -v /data:/data -i -t self/redis
