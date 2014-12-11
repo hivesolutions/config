da3eca5dfe1f8ec595b37a92ed3b8436c29c1fbf
joamag <joamag@gmail.com>
2014-12-11 17:37:45 +0000

    new instances import

--- instances/base/mysql.docker.sh
+++ instances/base/mysql.docker.sh
@@ -3,8 +3,12 @@
 
 set -e +h
 
+HOST=${HOST-172.17.42.1}
+PORT=${PORT-3306}
+
 mkdir mysql && cd mysql
 wget https://github.com/hivesolutions/config/raw/master/docker/mysql/Dockerfile -O Dockerfile
 docker pull ubuntu && docker build --no-cache -t self/mysql .
 cd .. && rm -rf mysql
-docker run --name mysql -p 0.0.0.0:3306:3306 -v /data:/data -i -t -d self/mysql
+
+docker run --name mysql -p $HOST:$PORT:$PORT -v /data:/data -i -t -d self/mysql
--- instances/base/redis.docker.sh
+++ instances/base/redis.docker.sh
@@ -3,7 +3,12 @@
 
 set -e +h
 
+HOST=${HOST-172.17.42.1}
+PORT=${PORT-6379}
+
 mkdir redis && cd redis
 wget https://github.com/hivesolutions/config/raw/master/docker/redis/Dockerfile
 docker build --no-cache -t self/redis .
 cd .. && rm -rf redis
+
+docker run --name redis -p $HOST:$PORT:$PORT -v /data:/data -i -t self/redis
