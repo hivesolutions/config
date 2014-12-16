77419af55499d5823af2a8cd6c33b50e919c2aea
joamag <joamag@gmail.com>
2014-12-16 18:12:18 +0000

    new files

---
+++ taurus/base/redis.docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+set -e +h
+
+REDIS_HOST=${REDIS_HOST-172.17.42.1}
+REDIS_PORT=${REDIS_PORT-6379}
+
+mkdir redis && cd redis
+wget https://github.com/hivesolutions/config/raw/master/docker/redis/Dockerfile
+docker build --no-cache -t self/redis .
+cd .. && rm -rf redis
+
+docker run --name redis -p $REDIS_HOST:$REDIS_PORT:$REDIS_PORT -v /data:/data -i -t self/redis
