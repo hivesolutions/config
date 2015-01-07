2a25cb1b0db483d04f86bed9de0626c9e4ac8128
joamag <joamag@gmail.com>
2015-01-07 11:44:59 +0000

    new variables

--- torus/docker.take_the_bill.proxy/build.sh
+++ torus/docker.take_the_bill.proxy/build.sh
@@ -3,11 +3,16 @@
 
 set -e +h
 
-REDIS_HOST=${REDIS_HOST-172.17.42.1}
-REDIS_PORT=${REDIS_PORT-6379}
+OMNI_DOMAIN=${OMNI_DOMAIN-omni.host}
+OMNI_HOST=${OMNI_HOST-172.17.42.1}
+OMNI_PORT=${OMNI_PORT-8080}
+TTB_DOMAIN=${TTB_DOMAIN-ttb.host}
+TTB_HOST=${TTB_HOST-172.17.42.1}
+TTB_PORT=${TTB_PORT-8181}
 
 mkdir take_the_bill.proxy && cd take_the_bill.proxy
 wget https://github.com/hivesolutions/config/raw/master/docker/redis/Dockerfile.proxy.tpl -O Dockerfile.tpl
+context Dockerfile.tpl > Dockerfile && rm Dockerfile.tpl
 docker build --no-cache -t self/take_the_bill.proxy .
 cd .. && rm -rf take_the_bill.proxy
 
