5845fae779e349ea623651e19c96772c9599aa8f
joamag <joamag@gmail.com>
2015-01-07 11:49:11 +0000

    new take the bill code change

--- docker/take_the_bill/Dockerfile.proxy.tpl
+++ docker/take_the_bill/Dockerfile.proxy.tpl
@@ -14,7 +14,5 @@ ADD proxy.py /proxy.py
 
 RUN apt-get update && apt-get install -y -q python python-setuptools wget
 RUN easy_install pip && pip install netius
-RUN wget https://github.com/hivesolutions/config/raw/master/take_the_bill/proxy.py.tpl
-context proxy.py.tpl > proxy.py && rm proxy.py.tpl
 
 ENTRYPOINT ["usr/bin/python", "proxy.py"]
--- torus/docker.take_the_bill.proxy/build.sh
+++ torus/docker.take_the_bill.proxy/build.sh
@@ -11,7 +11,9 @@ TTB_HOST=${TTB_HOST-172.17.42.1}
 TTB_PORT=${TTB_PORT-8181}
 
 mkdir take_the_bill.proxy && cd take_the_bill.proxy
-wget https://github.com/hivesolutions/config/raw/master/docker/redis/Dockerfile.proxy.tpl -O Dockerfile.tpl
+wget https://github.com/hivesolutions/config/raw/master/take_the_bill/proxy.py.tpl
+context proxy.py.tpl > proxy.py && rm proxy.py.tpl
+wget https://github.com/hivesolutions/config/raw/master/docker/take_the_bill/Dockerfile.proxy.tpl -O Dockerfile.tpl
 context Dockerfile.tpl > Dockerfile && rm Dockerfile.tpl
 docker build --no-cache -t self/take_the_bill.proxy .
 cd .. && rm -rf take_the_bill.proxy
