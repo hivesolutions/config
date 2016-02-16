46de7be2cd690b7158d98d425a624cb5139685e9
joamag <joamag@hive.pt>
2016-02-16 13:12:13 +0000

    new proxy value

--- omni/proxy.py
+++ omni/proxy.py
@@ -5,21 +5,18 @@ import re
 
 import netius.extra
 
+BASE_PORT = netius.conf("BASE_PORT", 8080, cast = int)
+NUMBER_PROCESSES = netius.conf("NUMBER_PROCESSES", 16, cast = int)
+NODE_TEMPLATE = netius.conf("NODE_TEMPLATE", "http://172.17.0.1:%d")
+
 if __name__ == "__main__":
+    nodes = []
+
+    for port in range(BASE_PORT, BASE_PORT + NUMBER_PROCESSES):
+        api_nodes.append(NODE_TEMPLATE % port)
+
     regex = (
-        (
-            re.compile(r"https://*"),
-            (
-                "http://172.17.0.1:8080",
-                "http://172.17.0.1:8181",
-                "http://172.17.0.1:8282",
-                "http://172.17.0.1:8383",
-                "http://172.17.0.1:8484",
-                "http://172.17.0.1:8585",
-                "http://172.17.0.1:8686",
-                "http://172.17.0.1:8787"
-            )
-        ),
+        (re.compile(r"https://*"), nodes),
     )
     server = netius.extra.ReverseProxyServer(regex = regex)
     server.serve(env = True)
