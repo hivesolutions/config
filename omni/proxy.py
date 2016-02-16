98c1dc22e6b7be56bef403386dc3d94f68cb6062
joamag <joamag@hive.pt>
2016-02-16 13:31:31 +0000

    new auth password

--- omni/proxy.py
+++ omni/proxy.py
@@ -8,6 +8,7 @@ import netius.extra
 BASE_PORT = netius.conf("BASE_PORT", 8080, cast = int)
 NUMBER_PROCESSES = netius.conf("NUMBER_PROCESSES", 8, cast = int)
 NODE_TEMPLATE = netius.conf("NODE_TEMPLATE", "http://172.17.0.1:%d")
+AUTH_PASSWORD = netius.conf("AUTH_PASSWORD", None)
 
 if __name__ == "__main__":
     nodes = []
@@ -17,8 +18,15 @@ if __name__ == "__main__":
 
     nodes = tuple(nodes)
 
+    if AUTH_PASSWORD: auth = netius.SimpleAuth(password = AUTH_PASSWORD)
+    else: auth = netius.AllowAuth()
+
     regex = (
         (re.compile(r"https://*"), nodes),
+    )
+    auth_regex =  = (
+        (re.compile(r"https://cert\.*"), netius.AllowAuth()),
+        (re.compile(r"https://*") auth) 
     )
     server = netius.extra.ReverseProxyServer(regex = regex)
     server.serve(env = True)
