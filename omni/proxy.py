2e2947c4dd134d2bcf8bda1997d3892c0c9978fa
joamag <joamag@hive.pt>
2016-09-01 11:47:39 +0100

    new proxy file

--- omni/proxy.py
+++ omni/proxy.py
@@ -11,30 +11,32 @@ NODE_TEMPLATE = netius.conf("NODE_TEMPLATE", "http://172.17.0.1:%d")
 AUTH_PASSWORD = netius.conf("AUTH_PASSWORD", None)
 AUTH_ADDRESSES = netius.conf("AUTH_ADDRESSES", [], cast = list)
 
-if __name__ == "__main__":
-    nodes = []
+nodes = []
 
-    for port in range(BASE_PORT, BASE_PORT + NUMBER_PROCESSES):
-        nodes.append(NODE_TEMPLATE % port)
+for port in range(BASE_PORT, BASE_PORT + NUMBER_PROCESSES):
+    nodes.append(NODE_TEMPLATE % port)
 
-    nodes = tuple(nodes)
-    auth_tuple = []
+nodes = tuple(nodes)
+auth_tuple = []
 
-    if AUTH_PASSWORD: auth_password = netius.SimpleAuth(password = AUTH_PASSWORD)
-    else: auth_password = None
-    if AUTH_ADDRESSES: auth_address = netius.AddressAuth(AUTH_ADDRESSES)
-    else: auth_address = None
+if AUTH_PASSWORD: auth_password = netius.SimpleAuth(password = AUTH_PASSWORD)
+else: auth_password = None
+if AUTH_ADDRESSES: auth_address = netius.AddressAuth(AUTH_ADDRESSES)
+else: auth_address = None
 
-    if auth_password: auth_tuple.append(auth_password)
-    if auth_address: auth_tuple.append(auth_address)
-    auth_tuple = tuple(auth_tuple) if auth_tuple else None
+if auth_password: auth_tuple.append(auth_password)
+if auth_address: auth_tuple.append(auth_address)
+auth_tuple = tuple(auth_tuple) if auth_tuple else None
 
-    regex = (
-        (re.compile(r"https://*"), nodes),
-    )
-    auth_regex = (
-        (re.compile(r"https://[^/]+/omni/*"), None),
-        (re.compile(r"https://*"), auth_tuple)
-    )
-    server = netius.extra.ReverseProxyServer(regex = regex, auth_regex = auth_regex)
-    server.serve(env = True)
+regex = (
+    (re.compile(r"https://*"), nodes),
+)
+auth_regex = (
+    (re.compile(r"https://[^/]+/omni/*"), None),
+    (re.compile(r"https://*"), auth_tuple)
+)
+server = netius.extra.ReverseProxyServer(
+    regex = regex,
+    auth_regex = auth_regex,
+)
+server.serve(env = True)
